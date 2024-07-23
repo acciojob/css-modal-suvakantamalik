219d33519d737722e3a99ad63b4468ba17863f25
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-07-23 22:18:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+document.getElementById('openModal').onclick = function() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+// Close the modal when clicking outside of it or on the close button
+window.onclick = function(event) {
+  var modal = document.getElementById('myModal');
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+document.querySelector('.close-modal').onclick = function() {
+  document.getElementById('myModal').style.display = "none";
+}
